a3a2162df5bc634f0a1705ff10fbe27469867f2b
Pedro <65288010+Pedro-Magalhaes-Coding@users.noreply.github.com>
2022-05-14 18:30:30 +0100

    Add files via upload

---
+++ pap/Login.js
@@ -0,0 +1,10 @@
+function Login()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+}
+if(username=="admin"&& password==123);
+{
+    alert("");
+    location.href = "páginainicial.html"
+}
