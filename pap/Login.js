3d8962fb5e30f344a35b29ae605330fcd1ce846e
Pedro <65288010+Pedro-Magalhaes-Coding@users.noreply.github.com>
2022-06-02 21:04:30 +0100

    Add files via upload

---
+++ pap/Login.js
@@ -0,0 +1,19 @@
+function Login() {
+    var username = document.getElementsByTagName("input")[0].value
+    var password = document.getElementsByTagName("input")[1].value
+        if(username== 'admin' && password==123){
+        alert("Login Efetuado");
+        location.href = "paginainicial.html"
+        }
+        else{
+            alert("Login Incorreto")
+            document.getElementsByTagName("input")[0].value = ""
+            document.getElementsByTagName("input")[1].value = ""
+            document.getElementsByTagName("input")[0].focus()
+        }     
+}
+
+function move(){
+    document.getElementsByTagName("input")[0].style.marginBottom = "30px"
+    document.getElementsByTagName("input")[0].style.transition = "1s"
+}
